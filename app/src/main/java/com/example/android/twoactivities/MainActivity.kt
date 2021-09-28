e93052572cb62e88bed27d2a37a1522d39d0b9b2
VadimSurits <sourits-moscow@mail.ru>
2021-09-28 15:01:47 +1000

    General part of Task 2.2 finished (part 9. Homework will be completed in additional repository: Andersen-HW2-Task2.2-part9)

--- app/src/main/java/com/example/android/twoactivities/MainActivity.kt
+++ app/src/main/java/com/example/android/twoactivities/MainActivity.kt
@@ -21,9 +21,60 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(LOG_TAG, "-------")
+        Log.d(LOG_TAG, "onCreate")
+
         mMessageEditText = findViewById(R.id.editText_main)
         mReplyHeadTextView = findViewById(R.id.text_header_reply)
         mReplyTextView = findViewById(R.id.text_message_reply)
+
+        if (savedInstanceState != null) {
+            val isVisible = savedInstanceState.getBoolean("reply_visible")
+            if (isVisible) {
+                mReplyHeadTextView.visibility = View.VISIBLE
+                mReplyTextView.text = savedInstanceState.getString("reply_text")
+                mReplyTextView.visibility = View.VISIBLE
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(LOG_TAG, "onStart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(LOG_TAG, "onPause")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(LOG_TAG, "onRestart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(LOG_TAG, "onResume")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        if (mReplyHeadTextView.visibility == View.VISIBLE) {
+            outState.putBoolean("reply_visible", true)
+            outState.putString("reply_text", mReplyTextView.text.toString())
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(LOG_TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(LOG_TAG, "onDestroy")
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
--- app/src/main/java/com/example/android/twoactivities/SecondActivity.kt
+++ app/src/main/java/com/example/android/twoactivities/SecondActivity.kt
@@ -2,6 +2,7 @@ package com.example.android.twoactivities
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -10,6 +11,7 @@ import android.view.View
 const val EXTRA_REPLY = "com.example.android.twoactivities.extra.REPLY"
 
 class SecondActivity : AppCompatActivity() {
+    private val LOG_TAG = SecondActivity::class.simpleName
     private lateinit var mReply: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,6 +22,38 @@ class SecondActivity : AppCompatActivity() {
         val message = intent.getStringExtra(EXTRA_MESSAGE)
         val textView: TextView = findViewById(R.id.text_message)
         textView.setText(message)
+        Log.d(LOG_TAG, "-------")
+        Log.d(LOG_TAG, "onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(LOG_TAG, "onStart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(LOG_TAG, "onPause")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(LOG_TAG, "onRestart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(LOG_TAG, "onResume")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(LOG_TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(LOG_TAG, "onDestroy")
     }
 
     fun returnReply(view: View) {
@@ -27,6 +61,7 @@ class SecondActivity : AppCompatActivity() {
         val replyIntent = Intent()
         replyIntent.putExtra(EXTRA_REPLY, reply)
         setResult(RESULT_OK, replyIntent)
+        Log.d(LOG_TAG, "End SecondActivity")
         finish()
     }
 }
